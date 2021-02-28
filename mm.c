9c80ecf478c12d154012b0450a915291b39c89a2
Robert Maxwell Ramstad <mmr393@e5-cse-204-05.cse.psu.edu>
2021-02-28 18:53:08 -0500

    All traces except for largemem-short

--- mm.c
+++ mm.c
@@ -1,7 +1,7 @@
 /*
  * mm.c
  *
- * Name: [FILL IN]
+ * Name: Robert Ramstad
  *
  * NOTE TO STUDENTS: Replace this header comment with your own header
  * comment that gives a high level description of your solution.
@@ -47,6 +47,12 @@
 #define memcpy mem_memcpy
 #endif /* DRIVER */
 
+/* Global Variables */
+#define WSIZE 8 /* Word and header/footer size (bytes) */
+#define DSIZE 16 /* Double word size (bytes) */
+#define CHUNKSIZE (1<<12) /* Extend heap by this amount (bytes) */
+static char* heap_listp = 0;
+
 /* What is the correct alignment? */
 #define ALIGNMENT 16
 
@@ -55,14 +61,150 @@ static size_t align(size_t x)
 {
     return ALIGNMENT * ((x+ALIGNMENT-1)/ALIGNMENT);
 }
+/* Textbook Macros */
 
+static size_t MIN(size_t x,size_t y){
+	return( ((x) < (y)? (x) : (y)) );
+}
+static size_t MAX(size_t x,size_t y){
+	return( ((x) > (y)? (x) : (y)) );
+}
+static size_t PACK(size_t size, int alloc){
+	return( ((size) | (alloc)) );
+}
+static size_t GET(void* p){ //function for get
+	return( (*(unsigned int *)(p)) );
+}
+
+static void PUT(void* p, size_t val) {
+	(*(unsigned int *)(p) = (val));
+}
+
+static size_t GET_SIZE(void* p){
+	return( (GET(p) & ~0x7) );
+}
+
+static size_t GET_ALLOC(void* p){
+	return( (GET(p) & 0x1) );
+}
+
+static void* HDRP(void* bp){
+	return( ((char *)(bp) - WSIZE) );
+}
+
+static void* FTRP(void* bp){
+	return( ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) );
+}
+
+static void* NEXT_BLKP(void* bp){
+	return( ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) );
+}
+
+static void* PREV_BLKP(void* bp){
+	return( ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE))) );
+}
+
+static void *coalesce(void *bp){ // boundary tag coalesching to merge blocks (from textbook)
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	if (prev_alloc && next_alloc) { /* Case 1 */
+		return bp;
+	}
+
+	else if (prev_alloc && !next_alloc) { /* Case 2 */
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size,0));
+	}
+	else if (!prev_alloc && next_alloc) { /* Case 3 */
+		size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+		PUT(FTRP(bp), PACK(size, 0));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
+
+	else { /* Case 4 */
+		size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+			GET_SIZE(FTRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
+	return bp;
+}
+
+static void *extend_heap(size_t words) //extends the heap with new free block(from textbook)
+{
+	char *bp;
+	size_t size;
+
+	/* Allocate an even number of words to maintain alignment */
+	size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+	if ((long)(bp = mem_sbrk(size)) == -1){
+		return NULL;}
+
+	/* Initialize free block header/footer and the epilogue header */
+	PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
+	PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
+
+	/* Coalesce if the previous block was free */
+	return coalesce(bp);
+}
+
+static void* find_fit(size_t asize){ //Finds a fitting block of size "asize" (from textbook)
+	/* First fit search */
+	void *bp;
+
+	for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+		if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+			return bp;
+		}
+	}
+	return NULL;
+}
+
+static void place(void *bp, size_t asize)
+ {
+	size_t csize = GET_SIZE(HDRP(bp));
+
+	if ((csize - asize) >= (2*DSIZE)) {
+		PUT(HDRP(bp), PACK(asize, 1));
+		PUT(FTRP(bp), PACK(asize, 1));
+		bp = NEXT_BLKP(bp);
+		PUT(HDRP(bp), PACK(csize-asize, 0));
+		PUT(FTRP(bp), PACK(csize-asize, 0));
+	}
+	else {
+		PUT(HDRP(bp), PACK(csize, 1));
+		PUT(FTRP(bp), PACK(csize, 1));
+	}
+}
 /*
  * Initialize: returns false on error, true on success.
  */
 bool mm_init(void)
 {
     /* IMPLEMENT THIS */
-    return true;
+
+    /* Create the initial empty heap */
+	if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1){
+ 		return false;
+	}
+ 	PUT(heap_listp, 0); /* Alignment padding */
+ 	PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
+ 	PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
+ 	PUT(heap_listp + (3*WSIZE), PACK(0, 1)); /* Epilogue header */
+ 	heap_listp += (2*WSIZE);
+
+ 	/* Extend the empty heap with a free block of CHUNKSIZE bytes */
+ 	if (extend_heap(CHUNKSIZE/WSIZE) == NULL){
+ 		return false;
+ 	}
+ 	return true;
+    
 }
 
 /*
@@ -71,7 +213,36 @@ bool mm_init(void)
 void* malloc(size_t size)
 {
     /* IMPLEMENT THIS */
-    return NULL;
+	size_t asize; /* Adjusted block size */
+	size_t extendsize; /* Amount to extend heap if no fit */
+	char *bp;
+
+	/* Ignore spurious requests */
+	if (size == 0){
+		return NULL;
+		}
+
+	/* Adjust block size to include overhead and alignment reqs. */
+	if (size <= DSIZE){
+		asize = 2*DSIZE;
+		}
+	else{
+		asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+		}
+
+	/* Search the free list for a fit */
+	if ((bp = find_fit(asize)) != NULL) {
+		place(bp, asize);
+		return bp;
+	}
+
+	/* No fit found. Get more memory and place the block */
+	extendsize = MAX(asize,CHUNKSIZE);
+	if ((bp = extend_heap(extendsize/WSIZE)) == NULL){
+		return NULL;
+	}
+	place(bp, asize);
+	return bp;
 }
 
 /*
@@ -79,8 +250,12 @@ void* malloc(size_t size)
  */
 void free(void* ptr)
 {
-    /* IMPLEMENT THIS */
-    return;
+    /* IMPLEMENT THIS */ 
+	size_t size = GET_SIZE(HDRP(ptr));
+    
+	PUT(HDRP(ptr), PACK(size, 0));
+	PUT(FTRP(ptr), PACK(size, 0));
+	coalesce(ptr);
 }
 
 /*
@@ -88,23 +263,27 @@ void free(void* ptr)
  */
 void* realloc(void* oldptr, size_t size)
 {
-    /* IMPLEMENT THIS */
-    return NULL;
-}
 
-/*
- * calloc
- * This function is not tested by mdriver, and has been implemented for you.
- */
-void* calloc(size_t nmemb, size_t size)
-{
-    void* ptr;
-    size *= nmemb;
-    ptr = malloc(size);
-    if (ptr) {
-        memset(ptr, 0, size);
-    }
-    return ptr;
+	size_t oldsize;
+	char *newptr;
+    /* IMPLEMENT THIS */
+	if (oldptr == NULL){
+		return( malloc(size) );
+	}
+	
+	if (size == 0){
+		free(oldptr);
+	}
+	
+	oldsize = GET_SIZE(oldptr);
+	newptr = malloc(size);
+	mem_memcpy(newptr, oldptr, MIN(size,oldsize));
+	free(oldptr);
+	
+	return newptr;
+	
+	 
+	
 }
 
 /*
